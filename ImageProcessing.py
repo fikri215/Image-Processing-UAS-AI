9bcceda0e317fa6b5ffdc3e8324b1690c79b72e6
fikri215 <fikrimaulana215@gmail.com>
2020-11-20 23:27:38 +0700

    upload project

---
+++ ImageProcessing.py
@@ -0,0 +1,21 @@
+#***************Method 1: Using PILLOW (PIL)****************
+
+# importing PIL Module
+from PIL import Image
+
+# Read image
+img = Image.open('./saturn.jpg')
+
+# Display image
+img.show()
+
+
+
+
+
+
+
+
+
+
+
